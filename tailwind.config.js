f836a68d8d6c1591283736d4aac98977769f7d6f
Eric1K <ericfeng26@gmail.com>
2023-10-13 22:23:12 -0400

    graphs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/*.{html,js}"],
+  content: ["./pages/*.{html,js}","./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
+    require('flowbite/plugin')({
+      charts: true,
+  }),
   ],
 }
 
