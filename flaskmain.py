347beaa8f372f7d07e117d1d08d031922c4f9215
Eric1K <69730295+Eric1K@users.noreply.github.com>
2023-10-22 17:43:54 -0400

    flask basics

--- flaskmain.py
+++ flaskmain.py
@@ -1,7 +1,23 @@
 from flask import Flask
-
 app = Flask(__name__)
+"""
+Only run:
+python -m flask --app flaskmain run
 
+Run with making changes:
+python -m flask --app flaskmain run --debug
+"""
 @app.route("/")
+@app.route("/home")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<h1>Hello0s, World!</h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>About World!</h1>"
+
+
+"""
+if __name__ == "__main__":
+    app.run(debug=True)
+"""
