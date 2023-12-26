11babfe8df28a833091d047230bd33938acb6e9e
Tejas14-hub <tejasgbsb@gmail.com>
2023-12-26 14:06:01 -0500

    fixing minor bugs

--- flaskmain/models.py
+++ flaskmain/models.py
@@ -13,6 +13,8 @@ class Users(db.Model, UserMixin):
     # image_file = db.Column(db.String(20), nullable = False, default = "default.png")
     password = db.Column(db.String(60), nullable = False)
     #posts = db.relationship('Post', backref = "username", lazy = True) multiple posts
+    phone = db.Column(db.String(60), unique = True, nullable = False)
+    dob = db.Column(db.String(60), nullable = False)
 
     def __repr__(self):
         return f"Users('{self.name}', '{self.email}','{self.password}')"
